5c3b659cab35bbaa38eb454edfbdfb8a00c307b0
captnketchup <74988054+captnketchup@users.noreply.github.com>
2021-04-16 01:15:02 +0200

    Removed comments, fixed float operations.

--- Skeleton/Skeleton.cpp
+++ Skeleton/Skeleton.cpp
@@ -33,39 +33,6 @@
 //=============================================================================================
 #include "framework.h"
 
-//// vertex shader in GLSL: It is a Raw string (C++11) since it contains new line characters
-//const char *const vertexSource = R"(
-//	#version 330				// Shader 3.3
-//	precision highp float;		// normal floats, makes no difference on desktop computers
-//
-//	uniform mat4 MVP;			// uniform variable, the Model-View-Projection transformation matrix
-//	layout(location = 0) in vec2 vp;	// Varying input: vp = vertex position is expected in attrib array 0
-//
-//	void main() {
-//		gl_Position = vec4(vp.x, vp.y, 0, 1) * MVP;		// transform vp from modeling space to normalized device space
-//	}
-//)";
-//
-//// fragment shader in GLSL
-//const char *const fragmentSource = R"(
-//	#version 330			// Shader 3.3
-//	precision highp float;	// normal floats, makes no difference on desktop computers
-//	
-//	uniform vec3 color;		// uniform variable, the color of the primitive
-//	out vec4 outColor;		// computed color of the current pixel
-//
-//	void main() {
-//		outColor = vec4(color, 1);	// computed color is the color of the primitive
-//	}
-//)";
-
-
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//TODO : Pairt nem lehet használni !!!!!!  FONTOS
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
 // vertex shader in GLSL
 const char *vertexSource = R"(
 	#version 330
@@ -95,13 +62,12 @@ const char *fragmentSource = R"(
 )";
 
 
-GPUProgram gpuProgram; // vertex and fragment shaders
-unsigned int vao;	   // virtual world on the GPU
+GPUProgram gpuProgram; 
+unsigned int vao;	   
 const float EPSILON = 0.01f;
 bool isMouseDown = false;
 int mouseX;
 int mouseY;
-bool printDebug = false;
 
 template<typename T1, typename T2>
 class Pair {
@@ -115,23 +81,21 @@ public:
 	}
 };
 
-void vec3Print(std::string name, vec3 vector);
 Pair<float, float> quadraticEq(float a, float b, float c);
 bool hasRoot(float a, float b, float c);
 
 struct Material {
 	vec3 ka, kd, ks;
 	float shininess;
-	//Material(vec3 _kd, vec3 _ks, float _shininess, bool _isReflective) : ka(_kd *M_PI), kd(_kd), ks(_ks), shininess(_shininess) {};
 	Material(vec3 _kd, vec3 _ks, float _shininess, bool _isReflective) : ka(0.02f, 0.02f, 0.02f), kd(_kd), ks(_ks), shininess(_shininess) {};
 };
 
 struct Hit {
 	float t;
 	vec3 position, normal;
-	Material *material;
-	bool isReflective;
-	bool isPortal;
+	Material *material = nullptr;
+	bool isReflective = false;
+	bool isPortal = false;
 	Hit() { t = -1; }
 };
 
@@ -147,7 +111,7 @@ struct Ray {
 
 class Intersectable {
 protected:
-	Material *material;
+	Material *material = nullptr;
 public:
 	virtual Hit intersect(const Ray &ray) = 0;
 };
@@ -194,10 +158,7 @@ class Dodecahedron : public Intersectable {
 public:
 
 	Dodecahedron() {
-		//TODO: ha nagyon fucked a tükrözõdés akkor ezeket kell baszni
-		//vec3 kd(0.5f, 0.05f, 1.5f), ks(500.0f, 100.0f, 100.0f);
-		vec3 kd(0.592f, 0.0f, 0.639f), ks(0.8f, 0.2f, 0.4f);		//ks hogy melyik színt hogy csillantja meg
-		//material = new Material(kd, ks, 50, false);
+		vec3 kd(0.592f, 0.0f, 0.639f), ks(0.8f, 0.2f, 0.4f);		
 		material = new Material(kd, ks, 10, false);
 	}
 
@@ -207,35 +168,33 @@ public:
 		vec3 p3 = vertices[faces[faceIndex][2] - 1];
 
 		vec3 normal = cross(p2 - p1, p3 - p1);
-		if (dot(p1, normal) < 0)	//ha kifelé mutat meginvertáljuk
+		if (dot(p1, normal) < 0)	
 			normal = -normal;
-		//TODO: szkél??
 		return Pair<vec3, vec3>(p1, normal);
 	}
 
 	float distanceFromPlane(vec3 point, vec3 planePoint, vec3 planeNormal) {
-		return abs(dot(point - planePoint, planeNormal));
+		return fabs(dot(point - planePoint, planeNormal));
 	}
 
 	Hit intersect(const Ray &ray) {
 		Hit hit = Hit();
 		for (int i = 0; i < faces.size(); i++) {
 			Pair<vec3, vec3> planePair = getObjectPlane(i);
-			vec3 planePoint = planePair.first;	//first point of face
-			vec3 planeNormal = planePair.second;	//the normal of the plane
+			vec3 planePoint = planePair.first;	
+			vec3 planeNormal = planePair.second;	
 
-			float ti;	//distance on ray from start 'till first intersection
-			if (abs(dot(planeNormal, ray.dir)) > EPSILON) {
+			float ti;	
+			if (fabs(dot(planeNormal, ray.dir)) > EPSILON) {
 				ti = dot(planePoint - ray.start, planeNormal) / dot(planeNormal, ray.dir);
 			}
 			else {
 				ti = -1.0f;
 			}
 
-			if (ti <= EPSILON) continue;	//is current ti closer than hit.t?
+			if (ti <= EPSILON) continue;	
 			vec3 pintersect = ray.start + ray.dir * ti;
 
-			//now we check if intersection is outside of the face or not
 			bool outside = false;
 			bool reflective = true;
 			for (int j = 0; j < faces.size(); j++) {
@@ -243,9 +202,7 @@ public:
 				Pair<vec3, vec3> otherPlanePair = getObjectPlane(j);
 				vec3 otherPlanePoint = otherPlanePair.first;
 				vec3 otherPlaneNormal = otherPlanePair.second;
-				//printf("normal: %3.5f, %3.5f, %3.5f\npintersect: %3.5f, %3.5f, 3.5f\nother point: %3.5f, %3.5f, %3.5f\n", normal.x, normal.y, normal.z, pintersect.x, pintersect.y, pintersect.z, otherPoint.x, otherPoint.y, otherPoint.z);
-				if (dot(otherPlaneNormal, pintersect - otherPlanePoint) > 0) {	//checks if normalvector is pointing inwards
-					//printf("entered if(dot(normal, pintersect - otherPoint > 0))\n");
+				if (dot(otherPlaneNormal, pintersect - otherPlanePoint) > 0) {	
 					outside = true;
 					break;
 				}
@@ -259,7 +216,6 @@ public:
 				hit.normal = normalize(planeNormal);
 				hit.material = material;
 				hit.isReflective = reflective;
-				//printf("intersect");
 			}
 		}
 		return hit;
@@ -272,17 +228,15 @@ class Paraboloid : public Intersectable {
 	float radius = 0.3f;
 
 public:
-	// meg voltak adva ezek
 	vec3 n = { 0.17f, 0.35f, 1.5f };
 	vec3 kappa = { 3.1f, 2.7f, 1.9f };
 
 	Paraboloid() {
-		vec3 kd(1.0f, 0.945f, 0.360f), ks(1.0f, 1.0f, 1.0f);		//idk aranyszínû szerû
+		vec3 kd(1.0f, 0.945f, 0.360f), ks(1.0f, 1.0f, 1.0f);		
 
 		material = new Material(kd, ks, 50, true);
 	}
 	Hit intersect(const Ray &ray) {
-		//checks if intersects paraboloid
 		float a = expA * ray.dir.x * ray.dir.x +
 			expB * ray.dir.y * ray.dir.y;
 		float b = 2 * expA * ray.start.x * ray.dir.x +
@@ -305,25 +259,24 @@ public:
 		float tTest = -1.0;
 
 		if (dot(tempPosition1, tempPosition1) > (radius * radius))
-			t1 = -1.0f;		//t1 nem jo
+			t1 = -1.0f;		
 
 		if (dot(tempPosition2, tempPosition2) > (radius * radius))
-			t2 = -1.0f;		//t2 nem jo
+			t2 = -1.0f;		
 
 
-		if (t1 > 0) tTest = t1;		//t1 ervenyes
+		if (t1 > 0) tTest = t1;		
 
 		if (t2 > 0 && t2 < tTest) {
 			tTest = t2;
 		}
 
-		if (tTest <= 0) return Hit();		//idk hogy kell-e egyenlo
+		if (tTest <= 0) return Hit();		
 
 		Hit hit = Hit();
 		hit.t = tTest;
 		hit.position = ray.start + ray.dir * hit.t;
 
-		//https://aggie.io/ysax3oxo0k
 		vec3 focusPoint = { 0.0f, hit.position.y / (4 * (hit.position.x * hit.position.x + hit.position.z * hit.position.z)), 0.0f };
 		vec3 paraboloidA = hit.position - focusPoint;
 		vec3 paraboloidB = length(paraboloidA) * vec3(0.0f, 1.0f, 0.0f);
@@ -335,7 +288,6 @@ public:
 };
 
 
-//TODO: megérteni mi történik itt
 class Camera {
 	vec3 eye, lookat, right, up;
 	vec3 vup = { 0.0f, 1.0f, 0.0f };
@@ -355,9 +307,8 @@ public:
 	}
 	void Animate(float t)
 	{
-		//rotates the camera around lookat ( = (0, 0, 0))
 		float r = sqrtf(eye.x * eye.x + eye.y * eye.y);
-		eye = vec3(r * cos(t) + lookat.x, r * sin(t) + lookat.y, eye.z);
+		eye = vec3(r * cosf(t) + lookat.x, r * sinf(t) + lookat.y, eye.z);
 		set(eye, lookat);
 	}
 };
@@ -376,10 +327,8 @@ class Scene {
 	Dodecahedron dodecahedron;
 	Paraboloid paraboloid;
 	std::vector<Light *> lights;
-	//vec3 La = { 0.4f, 0.4f, 0.4f };
 	vec3 La = { 0.584f, 0.827f, 0.933f };
 
-	// returns the reflected ray's direction vector
 	vec3 reflect(vec3 inDirectionVec, vec3 normal) {
 		return inDirectionVec - normal * dot(normal, inDirectionVec) * 2.0f;
 	}
@@ -392,10 +341,7 @@ class Scene {
 		vec3 kappa = paraboloid.kappa;
 		F0 = ((n - one) * (n - one) + (kappa * kappa)) /
 			((n + one) * (n + one) + (kappa * kappa));
-		if (printDebug) {
-			printf("cosa: %3.5f\n", cosa);
-		}
-		return F0 + (one - F0) * powf(1 - cosa, 5);		//return F0 + (one - F0) * pow(1 - cosa, 5); volt de 1- nélkül jobban néz ki a pow
+		return F0 + (one - F0) * powf( cosa, 5);		
 	}
 
 public:
@@ -405,13 +351,9 @@ public:
 		camera.set(vec3(0.5f, 0.5f, 0.5f), vec3(0.0f, 0.0f, 0.0f));
 		dodecahedron = Dodecahedron();
 		paraboloid = Paraboloid();
-		//vec3 Le = { 2.0f, 2.0f, 2.0f };
 		vec3 Le = { 1.0f, 1.0f, 1.0f };
-		vec3 lightPosition = { 0.0f, 0.0f, 0.0f };			// ez nem lesz jó mert itt lesz a cucc középen
-		//vec3 lightPosition = { 0.2f, 0.2f, 0.2f };
+		vec3 lightPosition = { 0.0f, 0.0f, 0.0f };			
 		lights.push_back(new Light(lightPosition, Le));
-		//lights.push_back(new Light(lightDirection * (-1), Le));
-		//TODO: ellipsoid haha nem is az xDDDD
 	}
 
 	void render(std::vector<vec4> &image) {
@@ -420,18 +362,13 @@ public:
 		for (int Y = 0; Y < windowHeight; Y++) {
 #pragma omp parallel for
 			for (int X = 0; X < windowWidth; X++) {
-				if (isMouseDown && X == mouseX && Y == mouseY) printDebug = true;
-				else printDebug = false;
 				vec3 color = trace(camera.getRay(X, Y));
 				image[Y * windowWidth + X] = vec4(color.x, color.y, color.z, 1);
-				//image[Y * windowWidth + X] = ((X + Y) % 2) ? black : white;
 			}
 		}
 	}
 
-	// checks if shot out light intersects with something
 	bool shadowIntersect(Ray ray) {
-		//TODO: ezt szépítsd meg úristen de ronda
 		Hit hit;
 		if (dodecahedron.intersect(ray).t > 0) return true;
 		return false;
@@ -444,30 +381,23 @@ public:
 
 		Hit paraboloidHit = paraboloid.intersect(ray);
 		Hit dodecaHit = dodecahedron.intersect(ray);
-		vec3 outRadiance(0, 0, 0);		// the radiancy(kinda color) of a given point where the ray intersects
+		vec3 outRadiance(0.0f, 0.0f, 0.0f);		
 
 		if (dot(ray.dir, paraboloidHit.normal) < 0) paraboloidHit.normal = paraboloidHit.normal * (-1);
 		if (dot(ray.dir, dodecaHit.normal) < 0) dodecaHit.normal = dodecaHit.normal * (-1);
 
-		//intersects paraboloid
 		if (paraboloidHit.t >= 0 && paraboloidHit.t < dodecaHit.t) {
-			//printf("Intersected paraboloid\n");
 			vec3 reflectionDir = reflect(ray.dir, paraboloidHit.normal);
-			Ray reflectRay(paraboloidHit.position - paraboloidHit.normal * EPSILON, reflectionDir);
+			Ray reflectRay(paraboloidHit.position - paraboloidHit.normal * EPSILON, reflectionDir);		
 			outRadiance = outRadiance + trace(reflectRay, depth + 1) * Fresnel(ray.dir, paraboloidHit.normal);
 		}
-		//intersects dodeca
 		else if (dodecaHit.t >= 0) {
-			if (!dodecaHit.isReflective) { //if material is rough
+			if (!dodecaHit.isReflective) { 
 				outRadiance = dodecaHit.material->ka * La;
 				for (Light *light : lights) {
-					//TODO: epsilon
 					vec3 lightDirection = dodecaHit.position - light->position;
 					Ray shadowRay(dodecaHit.position + dodecaHit.normal * EPSILON, lightDirection);
-					//Ray shadowRay(hit.position + hit.normal * EPSILON, light->direction);
-					float cosTheta = dot(dodecaHit.normal, lightDirection);		// is used to determine whether light is coming behind from the object
-					//Hit shadowHit = firstIntersect(shadowRay);
-					// if there's no object between the light source and the given point
+					float cosTheta = dot(dodecaHit.normal, lightDirection);	
 					if (cosTheta > 0 && !shadowIntersect(shadowRay)) {
 						outRadiance = outRadiance + light->Le * dodecaHit.material->kd * cosTheta;
 						vec3 halfway = normalize(-ray.dir + lightDirection);
@@ -476,12 +406,11 @@ public:
 					}
 				}
 			}
-			else {	//enter portal
+			else {	
 				vec3 reflectionDir = reflect(ray.dir, dodecaHit.normal);
 				vec4 tempReflDir = { reflectionDir.x, reflectionDir.y, reflectionDir.z, 0.0f };
 				mat4 rotationMat = RotationMatrix(72.0f * M_PI/180.0f, dodecaHit.normal);
 				tempReflDir = tempReflDir * rotationMat;
-				//vec3 rotatedDir = RotationMatrix(72.0f, reflectionDir);
 				reflectionDir.x = tempReflDir.x;
 				reflectionDir.y = tempReflDir.y;
 				reflectionDir.z = tempReflDir.z;
@@ -499,96 +428,87 @@ public:
 		else {
 			return La;
 		}
-
-		//return vec3(1.0f, 0.0f, 0.0f);
-		//outRadiance = outRadiance + La; // we add the ambient light to it
-				// ezt találtam a rekurzív raytracelõs videóban 07:28, ekcsölli jól néz ki
 		return outRadiance;
 	}
 } scene;
 
 class FullScreenTexturedQuad {
-	//destruktor????
-	unsigned int vao;	// vertex array object id and texture id
+	unsigned int vao;	
 	Texture texture;
 public:
 	FullScreenTexturedQuad(int windowWidth, int windowHeight, std::vector<vec4> &image)
 		: texture(windowWidth, windowHeight, image)
 	{
-		glGenVertexArrays(1, &vao);	// create 1 vertex array object
-		glBindVertexArray(vao);		// make it active
+		glGenVertexArrays(1, &vao);	
+		glBindVertexArray(vao);		
 
-		unsigned int vbo;		// vertex buffer objects
-		glGenBuffers(1, &vbo);	// Generate 1 vertex buffer objects
+		unsigned int vbo;		
+		glGenBuffers(1, &vbo);
 
-		// vertex coordinates: vbo0 -> Attrib Array 0 -> vertexPosition of the vertex shader
-		glBindBuffer(GL_ARRAY_BUFFER, vbo); // make it active, it is an array
-		float vertexCoords[] = { -1, -1,  1, -1,  1, 1,  -1, 1 };	// two triangles forming a quad
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexCoords), vertexCoords, GL_STATIC_DRAW);	   // copy to that part of the memory which is not modified 
+		glBindBuffer(GL_ARRAY_BUFFER, vbo); 
+		float vertexCoords[] = { -1, -1,  1, -1,  1, 1,  -1, 1 };	
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexCoords), vertexCoords, GL_STATIC_DRAW);	  
 		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);     // stride and offset: it is tightly packed
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);     
 	}
 
 	void Draw() {
-		glBindVertexArray(vao);	// make the vao and its vbos active playing the role of the data source
+		glBindVertexArray(vao);	
 		gpuProgram.setUniform(texture, "textureUnit");
-		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);	// draw two triangles forming a quad
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);	
 	}
 };
 
 FullScreenTexturedQuad *fullScreenTexturedQuad;
 
 void sceneRender() {
-	// this will contain the pixel data
+	
 	std::vector<vec4> image(windowWidth * windowHeight);
 
 	scene.render(image);
 
-	// copy image to GPU as a texture
+	
 	fullScreenTexturedQuad = new FullScreenTexturedQuad(windowWidth, windowHeight, image);
 }
 
-// Initialization, create an OpenGL context
+
 void onInitialization() {
 	glViewport(0, 0, windowWidth, windowHeight);
-	Dodecahedron dodecahedron();
-	//TODO: create scene
+	Dodecahedron dodecahedron;
+	
 	scene = Scene();
 	scene.build();
 
 	sceneRender();
 
-	// create program for the GPU
+	
 	gpuProgram.create(vertexSource, fragmentSource, "outColor");
 }
 
-// Window has become invalid: Redraw
+
 void onDisplay() {
 	fullScreenTexturedQuad->Draw();
-	glutSwapBuffers(); // exchange buffers for double buffering
+	glutSwapBuffers();
 }
 
-// Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
-	if (key == 'd') glutPostRedisplay();         // if d, invalidate display, i.e. redraw
+	if (key == 'd') glutPostRedisplay();       
 }
 
-// Key of ASCII code released
+
 void onKeyboardUp(unsigned char key, int pX, int pY) {
 }
 
-// Move mouse with key pressed
-void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
-	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
+
+void onMouseMotion(int pX, int pY) {	
+
+	float cX = 2.0f * pX / windowWidth - 1;
 	float cY = 1.0f - 2.0f * pY / windowHeight;
-	printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
 }
 
-// Mouse click event
-void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
-	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
+
+void onMouse(int button, int state, int pX, int pY) {
+	float cX = 2.0f * pX / windowWidth - 1;	
 	float cY = 1.0f - 2.0f * pY / windowHeight;
 
 	mouseX = pX;
@@ -611,22 +531,17 @@ void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel co
 	}
 }
 
-// Idle event indicating that some time elapsed: do animation here
+
 void onIdle() {
-	float time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
+	float time = glutGet(GLUT_ELAPSED_TIME); 
 	scene.camera.Animate(time / 5000);
 
 	sceneRender();
 	glutPostRedisplay();
 }
 
-
-void vec3Print(std::string name, vec3 vector) {
-	printf("%s: X: %3.2f, Y: %3.2f, Z: %3.2f\n", name.c_str(), vector.x, vector.y, vector.z);
-}
-
 bool hasRoot(float a, float b, float c) {
-	return b * b - 4.0 * a * c > 0;
+	return b * b - 4.0f * a * c > 0;
 }
 
 Pair<float, float> quadraticEq(float a, float b, float c) {
